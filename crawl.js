edcf0ac1cf735473ad32044972946ec5cd3be383
ahmed magdy <ahmedmagdy492@yahoo.com>
2023-12-07 14:13:31 +0200

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,29 @@
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(currentURL) {
+  console.log(`activley crawling: ${currentURL}`);
+
+  try {
+    const response = await fetch(currentURL);
+
+    if(response.status > 399) {
+      console.log(`error in fetch with status code ${response.status} on page: ${currentURL}`);
+      return;
+    }
+
+    const contentType = response.headers.get("content-type");
+    if(!contentType.includes('text/html')) {
+      console.log(`error did not receive html but instead ${contentType} on page: ${currentURL}`);
+      return;
+    }
+
+    console.log(await response.text());
+  }
+  catch(err) {
+    console.log(`Error in fetch: ${err.message} while visting ${currentURL}`);
+  }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom  = new JSDOM(htmlBody);
@@ -44,5 +68,6 @@ function normalizeURL(urlStr) {
 
 module.exports = {
   normalizeURL,
-  getURLsFromHTML
+  getURLsFromHTML,
+  crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const { crawlPage, getURLsFromHTML } = require('./crawl.js');
+
+function main() {
+  if(process.argv.length < 3) {
+    console.log(`Usage: ${process.argv[1]} <url>`);
+    process.exit(1);
+  }
+
+  try {
+    const baseURL = new URL(process.argv[2]);
+    console.log(`starting to crawl ${baseURL}...`);
+
+    crawlPage(baseURL);
+  }
+  catch(err) {
+    console.log(err.message);
+  }
+}
+
+main()
